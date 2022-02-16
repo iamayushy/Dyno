4f404db55ebc84cee2e8d00e2807adb47ca225ec
iamayushy <ayushhuns@gmail.com>
2022-02-16 18:47:06 +0530

    final push

---
+++ app/src/main/java/com/cli/login/user.java
@@ -0,0 +1,15 @@
+package com.cli.login;
+
+public class user {
+
+    public String fullName, email, password;
+
+    public user() {
+    }
+
+    public user(String fullName, String email, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+    }
+}
